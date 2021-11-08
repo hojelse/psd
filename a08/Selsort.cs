97042dc0a9c7d93fcf6c4fc6ce693e09b25a3ade
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2021-11-08 14:55:54 +0100

    Fix indentation

--- a08/Selsort.cs
+++ a08/Selsort.cs
@@ -7,9 +7,12 @@ class Selsort {
   public static void Main(String[] args) {
     int count = 10;
     int[] arr = new int[count];
+
     for (int i=0; i<count; i++)
       arr[i] = rnd.Next(1000000);
+
     SelectionSort(arr);
+
     for (int i=0; i<count; i++)
       Console.Write(arr[i] + " ");
     Console.WriteLine();
@@ -20,10 +23,14 @@ class Selsort {
   public static void SelectionSort(int[] arr) {
     for (int i = 0; i < arr.Length; i++) {
       int least = i;
+
       for (int j = i+1; j < arr.Length; j++)
         if (arr[j] < arr[least])
           least = j;
-      int tmp = arr[i]; arr[i] = arr[least]; arr[least] = tmp;
+
+      int tmp = arr[i];
+      arr[i] = arr[least];
+      arr[least] = tmp;
     }
   }
 }
